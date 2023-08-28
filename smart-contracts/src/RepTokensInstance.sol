be5d33c1de666fd55bf44367925ae17aa5d051be
Jacob Homanics <homanicsjake@gmail.com>
2023-08-28 15:59:04 -0500

    added in read me, removed counter, updated remappings

--- smart-contracts/src/RepTokensInstance.sol
+++ smart-contracts/src/RepTokensInstance.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {RepTokens} from "../lib/reputation/src/RepTokens.sol";
+import {RepTokens} from "@atxdao/contracts/reputation/RepTokens.sol";
 
 contract RepTokensInstance is RepTokens {
     constructor(address[] memory admins, uint256 maxMintAmountPerTx, string memory baseURI)
