e6934469997c460c6aa0c33f21486a3ed41b13a9
Jacob Homanics <homanicsjake@gmail.com>
2023-09-14 21:54:25 -0500

    latest - needs some work

--- packages/nextjs/pages/example-ui.tsx
+++ packages/nextjs/pages/example-ui.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import { MetaHeader } from "~~/components/MetaHeader";
+import { ContractData } from "~~/components/example-ui/ContractData";
 
-// import { ContractData } from "~~/components/example-ui/ContractData";
 // import { ContractInteraction } from "~~/components/example-ui/ContractInteraction";
 
 const ExampleUI: NextPage = () => {
@@ -16,8 +16,8 @@ const ExampleUI: NextPage = () => {
         <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
       </MetaHeader>
       <div className="grid lg:grid-cols-2 flex-grow" data-theme="exampleUi">
-        {/* <ContractInteraction />
-        <ContractData /> */}
+        {/* <ContractInteraction /> */}
+        <ContractData />
       </div>
     </>
   );
