044d00ab98923bd190392b0a4eb909f51b4d4685
Jacob Homanics <homanicsjake@gmail.com>
2023-11-29 02:10:12 -0600

    set up test script with test contracts

--- packages/foundry/script/DeployRepTokensInstanceWithData.s.sol
+++ packages/foundry/script/DeployRepTokensInstanceWithData.s.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.13;
 
 import {RepTokensInstance} from "../contracts/RepTokensInstance.sol";
 import {Script, console} from "../lib/forge-std/src/Script.sol";
+import {TokenTypesStorage} from "@atxdao/contracts/reputation/storage/TokenTypesStorage.sol";
+import {IReputationTokensInternal} from "@atxdao/contracts/reputation/interfaces/IReputationTokensInternal.sol";
 
 contract DeployRepTokensInstanceWithData is Script {
     address[] _admins = [0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266];
@@ -19,6 +21,40 @@ contract DeployRepTokensInstanceWithData is Script {
             _admins,
             baseURI
         );
+
+        instance.grantRole(
+            instance.TOKEN_TYPE_CREATOR_ROLE(),
+            0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+        );
+
+        instance.grantRole(
+            instance.MINTER_ROLE(),
+            0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+        );
+
+        instance.grantRole(
+            instance.DISTRIBUTOR_ROLE(),
+            0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+        );
+
+        TokenTypesStorage.TokenType memory tokenType = TokenTypesStorage
+            .TokenType(true, 100);
+
+        instance.createTokenType(tokenType);
+        instance.createTokenType(tokenType);
+
+        IReputationTokensInternal.TokenOperation[]
+            memory ops = new IReputationTokensInternal.TokenOperation[](2);
+        IReputationTokensInternal.TokenOperation
+            memory tokenOp1 = IReputationTokensInternal.TokenOperation(0, 90);
+        IReputationTokensInternal.TokenOperation
+            memory tokenOp2 = IReputationTokensInternal.TokenOperation(1, 90);
+
+        ops[0] = tokenOp1;
+        ops[1] = tokenOp2;
+
+        instance.mint(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, ops, "");
+
         vm.stopBroadcast();
         return instance;
     }
