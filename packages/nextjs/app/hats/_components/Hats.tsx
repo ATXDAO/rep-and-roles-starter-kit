092b4be5442badc2507a4dd3cc3c48f064c215c8
Jacob Homanics <homanicsjake@gmail.com>
2024-03-28 02:53:21 -0500

    Update Hats.tsx

--- packages/nextjs/app/hats/_components/Hats.tsx
+++ packages/nextjs/app/hats/_components/Hats.tsx
@@ -79,6 +79,7 @@ export function Hats() {
     <>
       <div className="py-5 space-y-5 flex flex-col justify-center items-center bg-primary bg-[length:100%_100%] py-1 px-5 sm:px-0 lg:py-auto max-w-[100vw] ">
         <p>{result?.data?.name}</p>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
           src={result?.data?.image.replace("ipfs://", "https://nftstorage.link/ipfs/")}
           width={156}
