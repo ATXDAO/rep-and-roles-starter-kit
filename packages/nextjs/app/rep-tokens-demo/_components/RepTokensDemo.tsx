f8427f8df414432e3b06383094fe543e275d2dfd
Jacob Homanics <homanicsjake@gmail.com>
2024-04-06 00:25:26 -0500

    added tabs to demo

--- packages/nextjs/app/rep-tokens-demo/_components/RepTokensDemo.tsx
+++ packages/nextjs/app/rep-tokens-demo/_components/RepTokensDemo.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 import { useAccount } from "wagmi";
 import { StylizedAddressCard } from "~~/components/rep-tokens/cards/stylized-cards/StylizedAddressCard";
 import { StylizedTokenCard } from "~~/components/rep-tokens/cards/stylized-cards/StylizedTokenCard";
@@ -42,59 +44,95 @@ export function RepTokensDemo() {
 
   return (
     <>
-      <div className="py-5 space-y-5 flex flex-col justify-center items-center bg-[length:100%_100%] py-1 px-5 sm:px-0 lg:py-auto max-w-[100vw] ">
-        <StylizedTokenGroupCard
-          tokens={faucetTokens}
-          components={["Balance", "Image"]}
-          isBalanceOverlayed={true}
-          size="xs"
-          preChildren={<p className="text-center text-4xl bg-base-200 rounded-lg">Faucet</p>}
-          postChildren={
-            <button
-              className="btn btn-primary btn-sm font-normal gap-1"
-              onClick={async () => {
-                await claim();
-                await refetchUserBalances();
-                await refetchFaucetBalances();
-                await refetchBalance();
-              }}
-            >
-              Claim Tokens
-            </button>
-          }
-        />
+      <Tabs>
+        <TabList>
+          <Tab>Multi-Card</Tab>
+          <Tab>Multi-Card w/ Overlay</Tab>
+          <Tab>Small</Tab>
+          <Tab>Widget</Tab>
+          <Tab>Single Card</Tab>
+          <Tab>Individual Components</Tab>
+          <Tab>Faucet</Tab>
+        </TabList>
 
-        <p className="text-center text-4xl">Individual Components</p>
-        <BalanceCard token={token} />
-        <ImageCard token={token} />
-        <NameCard token={token} />
-        <DescriptionCard token={token} />
-        <AddressCard token={token} />
-        <TokenTypeCard token={token} />
-        <MaxMintAmountPerTxCard token={token} />
+        <div className="py-5 space-y-5 flex flex-col justify-center items-center bg-[length:100%_100%] py-1 px-5 sm:px-0 lg:py-auto max-w-[100vw] ">
+          <TabPanel>
+            <p className="text-center text-4xl">Multi-Card</p>
+            <StylizedTokenGroupCard
+              tokens={tokens}
+              preChildren={<StylizedAddressCard address={tokens.address} isGroup={true} />}
+            />
+          </TabPanel>
 
-        <p className="text-center text-4xl">Single Card</p>
-        <StylizedTokenCard token={token} />
+          <TabPanel>
+            <p className="text-center text-4xl">Multi-Card w/ Overlay</p>
+            <StylizedTokenGroupCard
+              tokens={tokens}
+              isBalanceOverlayed={true}
+              preChildren={<StylizedAddressCard address={tokens.address} isGroup={true} />}
+            />
+          </TabPanel>
 
-        <p className="text-center text-4xl">Multi-Card</p>
-        <StylizedTokenGroupCard
-          tokens={tokens}
-          preChildren={<StylizedAddressCard address={tokens.address} isGroup={true} />}
-        />
+          <TabPanel>
+            <p className="text-center text-4xl">Small</p>
+            <StylizedTokenGroupCard
+              tokens={tokens}
+              components={["Balance", "Image"]}
+              isBalanceOverlayed={true}
+              size="sm"
+            />
+          </TabPanel>
 
-        <p className="text-center text-4xl">Multi-Card w/ Overlay</p>
-        <StylizedTokenGroupCard
-          tokens={tokens}
-          isBalanceOverlayed={true}
-          preChildren={<StylizedAddressCard address={tokens.address} isGroup={true} />}
-        />
+          <TabPanel>
+            <p className="text-center text-4xl">Widget</p>
+            <StylizedTokenGroupCard
+              tokens={tokens}
+              components={["Balance", "Image"]}
+              isBalanceOverlayed={true}
+              size="xs"
+            />
+          </TabPanel>
 
-        <p className="text-center text-4xl">Small</p>
-        <StylizedTokenGroupCard tokens={tokens} components={["Balance", "Image"]} isBalanceOverlayed={true} size="sm" />
+          <TabPanel>
+            <p className="text-center text-4xl">Single Card</p>
+            <StylizedTokenCard token={token} />
+          </TabPanel>
 
-        <p className="text-center text-4xl">Widget</p>
-        <StylizedTokenGroupCard tokens={tokens} components={["Balance", "Image"]} isBalanceOverlayed={true} size="xs" />
-      </div>
+          <TabPanel>
+            <p className="text-center text-4xl">Individual Components</p>
+            <BalanceCard token={token} />
+            <ImageCard token={token} />
+            <NameCard token={token} />
+            <DescriptionCard token={token} />
+            <AddressCard token={token} />
+            <TokenTypeCard token={token} />
+            <MaxMintAmountPerTxCard token={token} />
+          </TabPanel>
+
+          <TabPanel>
+            <StylizedTokenGroupCard
+              tokens={faucetTokens}
+              components={["Balance", "Image"]}
+              isBalanceOverlayed={true}
+              size="xs"
+              preChildren={<p className="text-center text-4xl bg-base-200 rounded-lg">Faucet</p>}
+              postChildren={
+                <button
+                  className="btn btn-primary btn-sm font-normal gap-1"
+                  onClick={async () => {
+                    await claim();
+                    await refetchUserBalances();
+                    await refetchFaucetBalances();
+                    await refetchBalance();
+                  }}
+                >
+                  Claim Tokens
+                </button>
+              }
+            />
+          </TabPanel>
+        </div>
+      </Tabs>
     </>
   );
 }
