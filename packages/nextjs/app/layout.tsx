345eaa1eb561ec5bb4bb30354aadeb3c284bbec1
Jacob Homanics <homanicsjake@gmail.com>
2024-05-18 03:10:45 -0500

    forced chain to optimism

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -5,8 +5,8 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
-  description: "Built with 🏗 Scaffold-ETH 2",
+  title: "Reputation & Roles",
+  description: "Onchain tracking of trust.",
 });
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
