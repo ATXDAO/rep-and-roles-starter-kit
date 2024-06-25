0c506e7d474617b71bbb567152c5c2a292f47466
Jacob Homanics <homanicsjake@gmail.com>
2024-06-24 21:17:32 -0500

    removed factory

--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -7,7 +7,8 @@ import {ScaffoldETHDeploy} from "./DeployHelpers.s.sol";
 import {Hats} from "../contracts/Hats/Hats.sol";
 
 import {DeployDemoScript} from "./DeployDemo.s.sol";
-import {DeployFactoryScript} from "./DeployFactory.s.sol";
+
+// import {DeployFactoryScript} from "./DeployFactory.s.sol";
 
 contract DeployScript is ScaffoldETHDeploy {
     error InvalidPrivateKey(string);
@@ -16,9 +17,8 @@ contract DeployScript is ScaffoldETHDeploy {
         DeployDemoScript deployer = new DeployDemoScript();
         deployer.run();
 
-        DeployFactoryScript factoryDeployer = new DeployFactoryScript();
-
-        factoryDeployer.run();
+        // DeployFactoryScript factoryDeployer = new DeployFactoryScript();
+        // factoryDeployer.run();
         exportDeployments();
     }
 
